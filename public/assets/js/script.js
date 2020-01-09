164216c9eaf13a4138cba8c4c465860d8d66413d
TheBrellis <brianroyalellis@gmail.com>
2020-01-08 20:53:53 -0500

    added click event to enter button that directs to ipsum.html

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,6 @@
 $(() => {
-  $('#welcome-button').on('click', () => {
-    console.log('hello');
+  $('#welcome-button').on('click', (event) => {
+    event.preventDefault();
+    window.location.replace('../public/ipsum.html');
   });
 });
