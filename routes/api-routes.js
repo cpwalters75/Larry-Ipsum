6c31d04f1916245aca674ab2dc226691fedb9455
jmccutchen <purplegrits@yahoo.com>
2020-01-09 19:33:03 -0500

    api routes set up

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,4 +2,14 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => res.json('Sample API get endpoint'));
 
+router.post('/api/ipsum', (req, res) => {
+
+})
+
+router.put('/api/hallOfLarry', (req, res) => {
+
+})
+
+
+
 module.exports = router;
