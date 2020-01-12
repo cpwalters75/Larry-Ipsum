76c71f22826e5dd4886cbc478bcab8b2ee5deb5f
jmccutchen <purplegrits@yahoo.com>
2020-01-11 20:22:03 -0500

    most of ipsum post is done, just can't get text value from #newLarry

--- public/assets/js/ipsum.js
+++ public/assets/js/ipsum.js
@@ -5,15 +5,27 @@ $(document).ready(() => {
 
     event.preventDefault();
 
-    $.get("/api/ipsum", function(data) {
-        console.log("clicked")
-        console.log(data)
-        
-        
+    $.get('/api/ipsum', (data) => {
+      console.log(data);
     });
   });
 
+  // below is for creating new Larrys, I can't seem to get the value from #newLarry
+  const larryText = $('#newLarry').val().trim();
+  const larryEntry = `${larryText} so, call it Larry.`;
+
+  function newLarry(event) {
+    event.preventDefault();
+    const larry = {
+      quote: larryEntry,
+    };
+    console.log(larryText);
+    $.post('/api/ipsum', larry);
+  }
+
+  $(document).on('click', '#larryCreate', newLarry);
 });
+
 // START OF FOUNDATION JAVASCRIPT ===========================================================================================
-$("[data-menu-underline-from-center] a").addClass("underline-from-center");
+$('[data-menu-underline-from-center] a').addClass('underline-from-center');
 
