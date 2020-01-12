7c8d05e8eff18750868c17fa6510867ee431db20
TheBrellis <brianroyalellis@gmail.com>
2020-01-11 19:57:06 -0500

    make text are into a p tag so that div extends when ipsum is generated

--- public/assets/js/ipsum.js
+++ public/assets/js/ipsum.js
@@ -1,21 +1,36 @@
 
 $(document).ready(() => {
 
+const getContent = function (conditions) {
+  $.get("/api/ipsum", function(data) {
+    let ipsum='';
+      data.forEach((line)=> {
+        ipsum += line.quote + " ";
+      });
+  $("#publish-ipsum").text(ipsum);
+  });
+}
 
+
+// START OF CLICK EVENTS ----------------------------------------------------------
   $('#ipsum-create').on('click', (event) => {
-
     event.preventDefault();
-
-    $.get("/api/ipsum", function(data) {
+    let conditions = {
+      paraLength : $("#para-length").val(),
+      paraQuantity : $("#para-quantity").val()
+    }
+    getContent(conditions);
+/*
+   $.get("/api/ipsum", function(data) {
       let ipsum=''
         data.forEach((line)=> {
           ipsum += line.quote + " ";
         })
     $("#publish-ipsum").val(ipsum);
     });
-
+*/
   });
- 
+
 // START OF FOUNDATION JAVASCRIPT ===========================================================================================
 $("[data-menu-underline-from-center] a").addClass("underline-from-center");
 });
