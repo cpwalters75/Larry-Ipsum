61be314d5bd407e2c1bbea386625f44156167f9a
TheBrellis <brianroyalellis@gmail.com>
2020-01-14 18:47:13 -0500

    class Build_Ipsums runs, page is redirected by click event,  data is returned by server

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -8,8 +8,9 @@ class Build_Ipsum {
     this.length = length
   }
 
-  setValues() {
+  setValue() {
     let paraQuantity = '';
+    let paraLength = '';
     switch(this.quantity) {
       case "one":
          paraQuantity = 1;
@@ -23,19 +24,19 @@ class Build_Ipsum {
       default:
          paraQuantity = 100;
     }
-
+  
     switch(this.length) {
       case "short":
-        let paraLength = 3;
+         paraLength = 3;
         break
       case "medium":
-        let paraLength = 5;
+         paraLength = 5;
         break
       case "long":
-        let paraLength = 7;
+         paraLength = 7;
         break
       default:
-        let paraLength = 1;
+         paraLength = 1;
     }
 
     this.setIpsumLength(paraQuantity,paraLength);
@@ -48,10 +49,16 @@ class Build_Ipsum {
   }
 
   queryData(ipsumLength) {
-    //query database here
+    db.ipsums.findAll({
+      include: [db.ipsums],
+    }).then((dbPost)=> {
+      res.json(dbPost);
+      console.log(dbPost);
+    }).catch((error) => {
+      console.error(error);
+    })
+    }
   }
-
-};
 
 /*
 router.get('/ipsum', (req, res) => {
@@ -69,45 +76,10 @@ router.get('/ipsum/:quantity/:length', (req, res) => {
   
   let quantity = req.params.quantity;
   let length = req.params.length;
+  let newIpsum = new Build_Ipsum(quantity,length);
 
- /*
-  if (quantity == "one") {
-    let paraQuantity = 1;
-    return paraQuantity;
-  }
-  else if (quantity == "three") {
-    let paraQuantity = 3;
-    return paraQuantity;
-  }
-  else if (quantity == "five") {
-    let paraQuantity = 5;
-    return paraQuantity;
-  }
-  else {
-    res.err(404);
-  }
-
-  if (length === "short") {
-    let paraLength = 3;
-    return paraLength;
-  } else if (length === "medium") {
-    let paraLength = 5;
-    return paraLength;
-  } else if (length === "long") {
-    let paraLength = 7;
-    return paraLength;
-  } else {
-    res.error(404);
-  }
-
-  let ipsum = paraQuantity * paraLength
-*/
-let newIpsum = new Build_Ipsum(quantity,length);
-
-newIpsum.setValues()
-
-
-  res.json("message recieved!");
+  //newIpsum.setValue();
+  res.json("this works");
 })
 
 
