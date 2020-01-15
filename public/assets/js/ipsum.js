254ae0b4ad4a6faa5fa382e9d473e67c04241f34
TheBrellis <brianroyalellis@gmail.com>
2020-01-14 20:47:24 -0500

    clicking submit button now generates ipusm based on conditions passed in the form

--- public/assets/js/ipsum.js
+++ public/assets/js/ipsum.js
@@ -9,11 +9,7 @@ const getContent = function (paraQuantity, paraLength) {
   console.log(queryString);
   $.get(queryString, function(data) {
     console.log(data);
-     // data.forEach((line)=> {
-       // ipsum += line.quote + " ";
-      //});
-  $("#publish-ipsum").text("hurray");
-  
+  $("#publish-ipsum").text(data);
   })
 };
 
@@ -25,34 +21,26 @@ const getContent = function (paraQuantity, paraLength) {
     let paraLength = $("#para-length").val();
     let paraQuantity = $("#para-quantity").val();
     getContent(paraQuantity, paraLength);
-/*
-   $.get("/api/ipsum", function(data) {
-      let ipsum=''
-        data.forEach((line)=> {
-          ipsum += line.quote + " ";
-        })
-    $("#publish-ipsum").val(ipsum);
-    });
-*/
   });
 
-  // below is for creating new Larrys, I can't seem to get the value from #newLarry
-  /*
-   const larryText = $('#new-larry').val().trim();
-   const larryEntry = $('#new-larry').val().trim() + 'so, call it Larry.';
 
-   function newLarry(event) {
-    event.preventDefault();
-    const larry = {
-       quote: larryEntry,
-    };
-    
-    $.post('/api/ipsum', larry);
-   }
 
-   $('#larry-create').on('click', newLarry);
 
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // START OF FOUNDATION JAVASCRIPT ===========================================================================================
 $('[data-menu-underline-from-center] a').addClass('underline-from-center');
 
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,85 +2,56 @@
 const router = require('express').Router();
 const db = require('../models');
 
-class Build_Ipsum {
-  constructor(quantity,length) {
-    this.quantity = quantity,
-    this.length = length
-  }
-
-  setValue() {
-    let paraQuantity = '';
-    let paraLength = '';
-    switch(this.quantity) {
-      case "one":
-         paraQuantity = 1;
-        break
-      case "three":
-         paraQuantity = 3;
-        break
-      case "five":
-         paraQuantity = 5;
-        break
-      default:
-         paraQuantity = 100;
-    }
-  
-    switch(this.length) {
-      case "short":
-          paraLength = 3;
-        break
-      case "medium":
-          paraLength = 5;
-        break
-      case "long":
-          paraLength = 7;
-        break
-      default:
-          paraLength = 1;
-    }
-
-    return this.setIpsumLength(paraQuantity,paraLength);
-
-  }
-
-  setIpsumLength(paraQuantity, paraLength) {
-      let ipsumLength = paraQuantity * paraLength;
-      return this.queryData(ipsumLength);
-  }
-
-  queryData(ipsumLength) {
-    return db.ipsums.findAll({});
-    }
-  }
-
-/*
-router.get('/ipsum', (req, res) => {
-  db.ipsums.findAll({
-    // include: [db.ipsums],
-  }).then((dbPost) => {
-    res.json(dbPost);
-    console.log(dbPost);
-  }).catch((error) => {
-    console.error(error);
-  });
-});
-*/
 router.get('/ipsum/:quantity/:length', (req, res) => {
   
   let quantity = req.params.quantity;
   let length = req.params.length;
-  let newIpsum = new Build_Ipsum(quantity,length);
 
-  newIpsum.setValue().then((dbPost) => {
-    res.json(dbPost);
-    console.log(dbPost);
+  let paraQuantity = '';
+  let paraLength = '';
+
+  switch(quantity) {
+    case "one":
+       paraQuantity = 1;
+      break
+    case "three":
+       paraQuantity = 3;
+      break
+    case "five":
+       paraQuantity = 5;
+      break
+    default:
+       paraQuantity = 100;
+  }
+
+  switch(length) {
+    case "short":
+        paraLength = 3;
+      break
+    case "medium":
+        paraLength = 5;
+      break
+    case "long":
+        paraLength = 7;
+      break
+    default:
+        paraLength = 1;
+  }
+
+  let ipsumLength = paraQuantity * paraLength;
+
+  db.ipsums.findAll({  order: [ db.Sequelize.fn( 'RAND' ) ], limit: ipsumLength }).then((dbPost) => {
+    let ipsum = '';
+    dbPost.forEach((line)=> {
+      ipsum += line.quote + " ";
+    });
+
+   res.json(ipsum);
+
   }).catch((error) => {
     console.error(error);
-  });;
-
-
-  //res.json("this works");
-})
+  });
+});
 
 
 
